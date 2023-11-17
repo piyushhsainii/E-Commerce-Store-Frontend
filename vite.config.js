baaddbbed9312a289f621cba391eee07d24948b9
Piyush <sainipiyush8860@gmail.com>
2023-11-17 19:15:03 +0530

    final commit?3

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ["@material-ui/lab/SpeedDial", "@material-ui/lab/SpeedDialAction","@material-ui/lab/Rating"
-    , "@material-ui/data-grid"
-    ],
-    },
-  },
-  plugins: [react()],
-  server:{
-    proxy:{
-      "/api": "http://192.168.1.10:5000"
+  // build: {
+  //   rollupOptions: {
+  //     external: ["@material-ui/lab/SpeedDial", "@material-ui/lab/SpeedDialAction","@material-ui/lab/Rating"
+  //   , "@material-ui/data-grid"
+  //   ],
+  //   },
+  // },
+  // plugins: [react()],
+  // server:{
+  //   proxy:{
+  //     "/api": "http://192.168.1.10:5000"
 
-    }
-  }
+  //   }
+  // }
 })
 

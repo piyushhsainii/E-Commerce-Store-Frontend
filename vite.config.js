f477f119c9868382896ef6881abb540835038104
Piyush <sainipiyush8860@gmail.com>
2023-12-13 00:46:26 +0530

    v5

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: ["@mui/lab/SpeedDial", "@mui/lab/SpeedDialAction","@mui/lab/Rating"
-    , "@mui/data-grid" , "@mui/core" , "@mui/icons" , "@mui/lab"
+    , "@mui/data-grid" , "@mui/core" , "@mui/icons" , "@mui/lab" ,"@mui/lab/TreeItem"
     ],
     },
   },

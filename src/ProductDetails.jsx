76756d8f290a8494328abae3323edab738201480
Piyush <sainipiyush8860@gmail.com>
2023-12-13 00:06:58 +0530

    reinstalled mui-lab2

--- src/ProductDetails.jsx
+++ src/ProductDetails.jsx
@@ -17,7 +17,7 @@ import {
     Button
 
 } from "@material-ui/core";
-import  Rating  from "@material-ui/lab/Rating";
+import { Rating } from '@mui/material'
 import { ADD_REVIEW_RESET } from "./constants/productConstans";
 
 
